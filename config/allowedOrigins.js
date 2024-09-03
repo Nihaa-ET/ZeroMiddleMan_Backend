b48089f56a2e2b0f1e301b91bf55d86de008756f
Nihaa-ET <Nihaa@Etpl2024>
2024-09-04 00:58:43 +0530

    merge

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins =[
-    'https://zeromiddleman.onrender.com',
-    'http://localhost:3000 ',
+    'https://directories.zeromiddleman.com'
   ]
 
   module.exports = allowedOrigins
